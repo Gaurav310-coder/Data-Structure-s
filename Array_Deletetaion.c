04e542f2d0e57953e978c470703c2e5560177ff1
GAURAV KADAM <112737236+iamsorry0310@users.noreply.github.com>
2024-02-09 22:46:06 +0530

    Update Array_Deletetaion.c

--- Array_Deletetaion.c
+++ Array_Deletetaion.c
@@ -1,14 +1,23 @@
 #include<stdio.h>
 
+void Display(int arr[],int size){
+    for(int i=0;i<size;i++){
+        printf("%d ",arr[i]);
+    }
+    printf("\n");
+}
 
-
-
-int main()
-{
-    int arr[]={1,2,3,4,5};
-    int size=sizeof(arr)/sizeof(int);
-    delete();
-    size-=1;
-    display(arr,size);
+void Delete(int arr[],int size, int index){
+    for(int i = index ;i < size;i++){
+        arr[i]=arr[i+1];
+    }
+}
+int main(){
+    int arr[]={1,2,2,3,4,5};
+    int s = sizeof(arr)/sizeof(int);
+    Display(arr,s);
+    Delete(arr,s,2);
+    s--;
+    Display(arr,s);
     return 0;
 }
